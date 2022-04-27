955dff67895be5ac8a4a051d22f470c82e78a1e7
Tsedexashu08 <ashutsedex@gmail.com>
2022-04-26 18:03:58 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,7 +17,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *              for Holberton project
  */
 typedef struct list_s
 {
@@ -23,13 +29,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(const char *);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print(void) __attribute__((constructor));
 
-#endif /*LISTS_H*/
-© 2022 GitHub, Inc.
-Terms
-Privacy
-
+#endif /* LISTS_H */
