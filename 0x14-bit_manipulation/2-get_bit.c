b6bff1a1b4d4fd0a589f503817b90cc760e25841
Tsedexashu08 <ashutsedex@gmail.com>
2022-05-14 09:58:45 -0700

    work

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,18 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
- * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ *
+ * Return: value of the bit, or -1 if there is an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int divisor, check;
 
-	if (index > 64)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-
-	hold = n >> index;
-
-	return (hold & 1);
+	divisor = 1 << index;
+	check = n & divisor;
+	if (check == divisor)
+		return (1);
+	return (0);
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,24 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * clear_bit - set the value of a bit to 0 at a given index
- * @n: pointer to decimal number to change
- * @index: index position to change
- * Return: 1 if it worked, -1 if error
+ * flip_bits - number of different bits between two numbers
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits you would need to flip
+ * to get from one number to another.
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int i;
-	unsigned int hold;
+	unsigned long int diff, check;
+	unsigned int count, i;
 
-	if (index > 64)
-		return (-1);
-	hold = index;
-	for (i = 1; hold > 0; i *= 2, hold--)
-		;
-
-	if ((*n >> index) & 1)
-		*n -= i;
-
-	return (1);
+	count = 0;
+	check = 1;
+	diff = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (check == (diff & check))
+			count++;
+		check <<= 1;
+	}
+	return (count);
 }
